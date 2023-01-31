2a8bb58f36bb9850171203dab5d22e01f49f150f
Ron Muth <ron.muth@isi-net.com>
2023-01-31 12:13:20 -0500

    WIP

--- src/ISI.ServiceExample.AssemblyInfo.cs
+++ src/ISI.ServiceExample.AssemblyInfo.cs
@@ -1,6 +1,6 @@
 #region Copyright & License
 /*
-Copyright (c) 2021, Integrated Solutions, Inc.
+Copyright (c) 2023 Integrated Solutions, Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -18,6 +18,6 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("ISI, Inc.")]
-[assembly: AssemblyCopyright("Copyright �ISIo, Inc. 2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Integrated Solutions, Inc.")]
+[assembly: AssemblyCopyright("Copyright (c) 2023, Integrated Solutions, Inc.")]
+[assembly: AssemblyTrademark("Integrated Solutions, Inc.")]
